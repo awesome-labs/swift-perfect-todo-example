96659c4b944c0ab6ae4286463a60dcc1ac2f79a1
Vinicius Souza <vinicius@codemakers.com.br>
2016-10-01 01:01:19 -0300

    add routes for creating new task and check as done. Edit css and html, add simple style.

--- Sources/Handlers.swift
+++ Sources/Handlers.swift
@@ -17,4 +17,49 @@ struct IndexHandler: MustachePageHandler{
   			response.completed()
   	}
   }
+
+}
+
+struct NewTaskHandler: MustachePageHandler{
+
+  func extendValuesForResponse(context contxt: MustacheWebEvaluationContext, collector: MustacheEvaluationOutputCollector) {
+    var values = MustacheEvaluationContext.MapType()
+    values["title"] = "Perfect Todo Example"
+    values["added"] = "Task added!"
+    let request = contxt.webRequest
+    if let taskName = request.param(name: "task_name"){
+      values["task-name"] = taskName
+    }
+    contxt.extendValues(with: values)
+    do {
+  			try contxt.requestCompleted(withCollector: collector)
+  	} catch {
+  			let response = contxt.webResponse
+  			response.status = .internalServerError
+  			response.appendBody(string: "\(error)")
+  			response.completed()
+  	}
+  }
+}
+
+struct TaskDoneHandler: MustachePageHandler{
+
+  func extendValuesForResponse(context contxt: MustacheWebEvaluationContext, collector: MustacheEvaluationOutputCollector) {
+    var values = MustacheEvaluationContext.MapType()
+    values["title"] = "Perfect Todo Example"
+    values["added"] = "Task added!"
+    let request = contxt.webRequest
+    if let taskName = request.param(name: "task_name"){
+      values["task-name"] = taskName
+    }
+    contxt.extendValues(with: values)
+    do {
+  			try contxt.requestCompleted(withCollector: collector)
+  	} catch {
+  			let response = contxt.webResponse
+  			response.status = .internalServerError
+  			response.appendBody(string: "\(error)")
+  			response.completed()
+  	}
+  }
 }
--- Sources/main.swift
+++ Sources/main.swift
@@ -27,6 +27,45 @@ routes.add(method: .get, uri: "/", handler: {
 	}
 )
 
+// Adding a route to handle the root list
+routes.add(method: .post, uri: "/new", handler: {
+	request, response in
+
+	// Setting the response content type explicitly to text/html
+	response.setHeader(.contentType, value: "text/html")
+	// Setting the body response to the generated list via Mustache
+
+	mustacheRequest(
+		request: request,
+		response: response,
+		handler: NewTaskHandler(),
+		templatePath: request.documentRoot + "/index.mustache"
+	)
+
+	// Signalling that the request is completed
+	response.completed()
+	}
+)
+
+// Adding a route to handle the root list
+routes.add(method: .post, uri: "/done", handler: {
+	request, response in
+
+	// Setting the response content type explicitly to text/html
+	response.setHeader(.contentType, value: "text/html")
+	// Setting the body response to the generated list via Mustache
+
+	mustacheRequest(
+		request: request,
+		response: response,
+		handler: TaskDoneHandler(),
+		templatePath: request.documentRoot + "/index.mustache"
+	)
+
+	// Signalling that the request is completed
+	response.completed()
+	}
+)
 // Add the routes to the server.
 server.addRoutes(routes)
 
@@ -37,7 +76,6 @@ server.serverPort = 8181
 // This is optional. If you do not want to serve static content then do not set this.
 // Setting the document root will automatically add a static file handler for the route /**
 server.documentRoot = "./webroot"
-
 
 do {
 	// Launch the HTTP server.
