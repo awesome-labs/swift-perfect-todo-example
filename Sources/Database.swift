0165f3d9ddfa1e572217a6c024b6aec5fe2b62b2
Vinicius Souza <vinicius@codemakers.com.br>
2016-10-10 12:44:45 -0300

    reuse mysql connection

--- Sources/Database.swift
+++ Sources/Database.swift
@@ -1,27 +1,29 @@
 import MySQL
 
 class DB {
-
+  var mysql = MySQL()
   let host = "127.0.0.1"
   let user = "root"
   let password = ""
   let db = "todo_perfect"
   let table = "todo"
+  var connected = false
+
+  init()  {
+    self.mysql = MySQL()
+    self.connected = self.mysql.connect(host: self.host, user: self.user, password: self.password, db: self.db)
+    if !self.connected {
+        print(mysql.errorMessage())
+    }
+  }
 
   func insertData(task: String) -> Bool {
-
     let status = 0
-    let mysql = MySQL()
-    let connected = mysql.connect(host: self.host, user: self.user, password: self.password, db: self.db)
-    guard connected else {
-        print(mysql.errorMessage())
+    guard self.connected else {
+        print(self.mysql.errorMessage())
         return false
     }
-
-    defer {
-        mysql.close()
-    }
-    return mysql.query(statement: "INSERT INTO \(self.table) (task, status) VALUES ('\(task)', \(status))")
+    return self.mysql.query(statement: "INSERT INTO \(self.table) (task, status) VALUES ('\(task)', \(status))")
   }
 
   func updateData(id: String?, status: Int) -> Bool {
@@ -29,39 +31,16 @@ class DB {
     guard let taskId = id else {
       return false
     }
-
-    let mysql = MySQL()
-    let connected = mysql.connect(host: self.host, user: self.user, password: self.password, db: self.db)
-    guard connected else {
-        print(mysql.errorMessage())
-        return false
-    }
-
-    defer {
-        mysql.close()
-    }
-    return mysql.query(statement: "UPDATE \(self.table) SET status=\(status) WHERE id=\(taskId)")
+    return self.mysql.query(statement: "UPDATE \(self.table) SET status=\(status) WHERE id=\(taskId)")
   }
 
   func fetchData() -> [[String: Any]]? {
 
-    let mysql = MySQL()
-    let connected = mysql.connect(host: self.host, user: self.user, password: self.password, db: self.db)
-    guard connected else {
-        print(mysql.errorMessage())
-        return nil
-    }
-
-    defer {
-        mysql.close()
-    }
-
-    let querySuccess = mysql.query(statement: "SELECT  * FROM todo ORDER BY id DESC LIMIT 10")
+    let querySuccess = self.mysql.query(statement: "SELECT  * FROM todo ORDER BY id DESC LIMIT 10")
     guard querySuccess else {
         return nil
     }
-
-    let results = mysql.storeResults()!
+    let results = self.mysql.storeResults()!
     var resultArray = [[String:Any]]()
 
     while let row = results.next() {
